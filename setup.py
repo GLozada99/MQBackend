3066b9fd5a4bce44c2737ac1d9b1090871bb8a7a
Gustavo Lozada <20170511@ce.pucmm.edu.do>
2022-01-25 14:53:25 -0400

    Add setup.py, update .gitignore
    The setup.py file allows to use pipenv install -e . to install the
    project as a package and facilitates importing its modules
    .gitignore now ignores files related to the package installed
    (MQBACKEND).

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MQBACKEND",
+    version="1.0.0",
+    description="Backend and DB for Mobile Quoter App",
+    packages=find_packages()  # __init__.py folders search
+)
